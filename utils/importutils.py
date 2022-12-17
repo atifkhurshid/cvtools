169fa08bdaa073e8a640594d3032512146e313b4
unknown <31736261+atifkhurshid@users.noreply.github.com>
2022-12-17 13:20:14 +0500

    Add lazy imports using imprtlib.util

---
+++ utils/importutils.py
@@ -0,0 +1,17 @@
+"""Lazy imports using importlib.util.LazyLoader
+
+Source: https://docs.python.org/3/library/importlib.html#importlib.util.LazyLoader
+"""
+import sys
+import importlib.util
+
+
+def lazy_import(name):
+    spec = importlib.util.find_spec(name)
+    loader = importlib.util.LazyLoader(spec.loader)
+    spec.loader = loader
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[name] = module
+    loader.exec_module(module)
+    
+    return module
