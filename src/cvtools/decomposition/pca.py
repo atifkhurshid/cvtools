7d44242543c633ca49e52a7083cf09569ec32b73
Atif Khurshid <31736261+atifkhurshid@users.noreply.github.com>
2025-06-21 22:56:04 +0200

    Update PCA module

--- src/cvtools/decomposition/pca.py
+++ src/cvtools/decomposition/pca.py
@@ -1,60 +1,45 @@
 """
-Package: decomposition.pca
-Requirements:
-    - sklearn
-Use: 
-    - from decomposition.pca import *
-Methods:
-    - None
-Class:
-    - PCA
-
-Author: Atif Khurshid
-
-Created: 2025-06-02
-Modified: None
-Version: 1.0
-
-Changelog:
-    - None
+Customized PCA module.
 """
-from tqdm import tqdm
+
+# Author: Atif Khurshid
+# Created: 2025-06-02
+# Modified: 2025-06-21
+# Version: 1.1
+# Changelog:
+#     - 2025-06-21: Change fit function to directly use fit function of IncrementalPCA
+
+import numpy as np
 from sklearn.decomposition import IncrementalPCA
 
 
 class PCA():
-
     def __init__(
             self,
-            n_components: int | str = "best",
             explained_variance_threshold: float = 0.95,
         ):
-
         self.pca = None
         self.explained_variance_threshold = explained_variance_threshold
         self.selected_components = None
     
 
-    def __call__(self, x):
-        self.transform(x)
+    def __call__(self, x: np.ndarray) -> np.ndarray:
+        return self.transform(x)
     
 
-    def fit(self, x, batch_size=1000):
-
+    def fit(
+            self,
+            x: np.ndarray,
+            batch_size: int | None = None,
+        ):
         n_samples, n_features = x.shape
-        self.pca = IncrementalPCA(n_components=n_features)
-        self.selected_components = None
-
-        batch_size = max(n_features + 1, batch_size)  # Ensure batch size is at least n_features + 1
-        for i in tqdm(range(0, n_samples, batch_size), total=n_samples // batch_size):
-            batch = x[i:i + batch_size]
-            self.pca.partial_fit(batch)
-
+        self.pca = IncrementalPCA(n_components=n_features, batch_size=batch_size)
+        self.pca.fit(x)
         cumulative_variance = self.pca.explained_variance_ratio_.cumsum()
         self.selected_components = self.pca.components_[cumulative_variance <= self.explained_variance_threshold]
 
 
-    def transform(self, x):
+    def transform(self, x: np.ndarray) -> np.ndarray:
         if self.pca:
             x -= self.pca.mean_
             x = x @ self.selected_components.T
