4ea19c76635347f736a50470559e8306cadcf467
Atif Khurshid <31736261+atifkhurshid@users.noreply.github.com>
2025-08-15 12:07:30 +0200

    Added support for transforms

--- src/cvtools/datasets/classification/features/saved_features_dataset_pt.py
+++ src/cvtools/datasets/classification/features/saved_features_dataset_pt.py
@@ -3,11 +3,11 @@ PyTorch Wrapper for saved features dataloader
 """
 
 # Author: Atif Khurshid
-# Created: 2022-08-08
-# Modified: None
-# Version: 1.0
+# Created: 2025-08-08
+# Modified: 2025-08-15
+# Version: 1.1
 # Changelog:
-#     - None
+#     - 2025-08-15: Added support for transforms.
 
 import torch
 from torch.utils.data import Dataset
@@ -27,7 +27,11 @@ class SavedFeaturesDatasetPT(SavedFeaturesDataset, Dataset):
             Positional arguments passed to the SavedFeaturesDataset constructor.
         **kwargs : dict
             Keyword arguments passed to the SavedFeaturesDataset constructor.
-
+            - transform: callable, optional
+                Transform to apply to the features.
+            - target_transform: callable, optional
+                Transform to apply to the labels.
+            
         Examples
         --------
         >>> dataset = SavedFeaturesDatasetPT("path/to/features")
@@ -40,6 +44,9 @@ class SavedFeaturesDatasetPT(SavedFeaturesDataset, Dataset):
         (32,)
         """
         super().__init__(*args, **kwargs)
+
+        self.transform = kwargs.pop("transform", None)
+        self.target_transform = kwargs.pop("target_transform", None)
 
 
     def __getitem__(self, index: int) -> tuple[torch.Tensor, torch.Tensor]:
@@ -60,5 +67,11 @@ class SavedFeaturesDatasetPT(SavedFeaturesDataset, Dataset):
 
         features = torch.tensor(features, dtype=torch.float32)
         labels = torch.tensor(labels, dtype=torch.long)
+
+        if self.transform is not None:
+            features = self.transform(features)
+
+        if self.target_transform is not None:
+            labels = self.target_transform(labels)
 
         return features, labels
