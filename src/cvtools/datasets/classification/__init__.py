51244f7dbb3629c3415ba58a68814da3ab1d0f2c
Atif Khurshid <31736261+atifkhurshid@users.noreply.github.com>
2025-09-05 13:36:10 +0200

    Fix import bug

--- src/cvtools/datasets/classification/__init__.py
+++ src/cvtools/datasets/classification/__init__.py
@@ -2,4 +2,4 @@ from .generic import ClassificationDataset
 from .cxr import CXRDataset
 from .sun import SUNDataset
 from .features import SavedFeaturesDataset
-from .scenes import ScenesDataset
+from .scenes import Scenes8Dataset, Scenes15Dataset
