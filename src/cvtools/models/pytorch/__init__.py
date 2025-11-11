8fb7b6afada50a80b4ac705fb7b3b82cea581efb
Atif Khurshid <31736261+atifkhurshid@users.noreply.github.com>
2025-11-11 11:13:47 +0100

    Add base CNN model for pytorch

--- src/cvtools/models/pytorch/__init__.py
+++ src/cvtools/models/pytorch/__init__.py
@@ -1,5 +1,6 @@
 from .base import PyTorchModel
 from .sequential import PyTorchSequentialModel
+from .cnn import PyTorchCNNModel
 from .utils import extract_features
 from .utils import evaluate_classification_model
 from .utils import train_classification_model
---
+++ src/cvtools/models/pytorch/cnn.py
@@ -0,0 +1,126 @@
+"""
+Base class for CNN models.
+"""
+
+# Author: Atif Khurshid
+# Created: 2025-11-11
+# Modified: 
+# Version: 1.0
+# Changelog:
+#     - 2025-11-11: Initial version.
+
+import math
+from typing import Callable
+
+import torch
+import torch.nn as nn
+
+from .base import PyTorchModel
+
+class PyTorchCNNModel(PyTorchModel):
+    
+    def __init__(self):
+        """
+        Base class for CNN models.
+        """
+        super().__init__()
+
+        self.activations = {}
+        self.features: nn.Module
+        self.avgpool: nn.Module
+        self.classifier: nn.Module
+
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass for the model.
+
+        The input is sequentially passed through the feature extractor,
+        average pooling, and classifier.
+
+        Parameters
+        ----------
+        x : torch.Tensor
+            Input tensor.
+
+        Returns
+        -------
+        torch.Tensor
+            Output tensor.
+        """
+        x = self.features(x)
+        x = self.avgpool(x)
+        x = self.classifier(x)
+
+        return x
+
+
+    def init_weights(self, a: float = 0, nonlinearity: str = 'relu'):
+        """
+        Initialize the weights of the model using Kaiming uniform initialization.
+
+        Parameters
+        ----------
+        a : float, optional
+            The negative slope of the rectifier used after this layer (only
+            used with 'leaky_relu'), by default 0.
+        nonlinearity : str, optional
+            The non-linear function used after convolutional layers, by default 'relu'.
+        """
+        for module in self.modules():
+            
+            bias = False
+            if isinstance(module, nn.Conv2d):
+                nn.init.kaiming_uniform_(module.weight, a=a, nonlinearity=nonlinearity)
+                if module.bias is not None:
+                    bias = True
+
+            if isinstance(module, nn.Linear):
+                nn.init.kaiming_uniform_(module.weight)
+                if module.bias is not None:
+                    bias = True
+
+            if bias:
+                fan_in, _ = nn.init._calculate_fan_in_and_fan_out(module.weight)
+                bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
+                nn.init.uniform_(module.bias, -bound, bound)
+
+
+    def register_hooks(self, modules_to_hook: list[str]):
+        """
+        Register forward hooks to capture activations from specified modules.
+
+        Parameters
+        ----------
+        modules_to_hook : list[str]
+            List of module names to register hooks on.
+        """
+        def get_hook(name: str) -> Callable:
+            def hook(
+                    module: nn.Module,
+                    input: torch.Tensor,
+                    output: torch.Tensor
+                ):
+                self.activations[name] = output.detach().cpu()
+                
+            return hook
+        
+        for name, module in self.named_modules():
+            if name in modules_to_hook:
+                module.register_forward_hook(get_hook(name))
+
+
+    def freeze_backbone(self):
+        """
+        Freeze the weights of the backbone feature extractor.
+        """
+        for param in self.features.parameters():
+            param.requires_grad = False
+
+
+    def unfreeze_backbone(self):
+        """
+        Unfreeze the weights of the backbone feature extractor.
+        """
+        for param in self.features.parameters():
+            param.requires_grad = True
