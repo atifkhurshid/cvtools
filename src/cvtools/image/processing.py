2b8d8682af4be2526c6bcdd94961ef8f952541f5
Atif Khurshid <31736261+atifkhurshid@users.noreply.github.com>
2026-03-04 10:23:34 +0100

    Update image processing

--- src/cvtools/image/processing.py
+++ src/cvtools/image/processing.py
@@ -4,8 +4,8 @@ Image processing module.
 
 # Author: Atif Khurshid
 # Created: 2022-12-18
-# Modified: 2026-03-03
-# Version: 1.6
+# Modified: 2026-03-04
+# Version: 1.7
 # Changelog:
 #     - 2025-05-22: Added fill and kwargs parameters to resize
 #     - 2025-05-22: Allowed resize without preserving aspect ratio
@@ -14,6 +14,9 @@ Image processing module.
 #     - 2025-10-29: Changed image processing library back to OpenCV
 #     - 2026-03-03: Added imresize_maximum function
 #     - 2026-03-03: Added imscale function
+#     - 2026-03-04: Refactored image padding into a separate function.
+
+from math import ceil, floor
 
 import cv2
 import numpy as np
@@ -59,7 +62,7 @@ def imresize(
         size: tuple[int, int],
         interpolation: int = cv2.INTER_LINEAR,
         preserve_aspect_ratio: bool = False,
-        fill: int = 0,
+        **kwargs: dict,
     ) -> np.ndarray:
     """
     Resize image possibly while maintaining aspect ratio.
@@ -74,8 +77,8 @@ def imresize(
         Interpolation method used for resizing, default is cv2.INTER_LINEAR.
     preserve_aspect_ratio : bool, optional
         Whether to preserve the aspect ratio of the image, default is False.
-    fill : int, optional
-        Value to use for padding when preserving aspect ratio, default is 0.
+    **kwargs : dict
+        Additional keyword arguments to pass to np.pad when preserving aspect ratio.
 
     Returns
     -------
@@ -87,27 +90,19 @@ def imresize(
     >>> from cvtools.image import imresize
     >>> import numpy as np
     >>> img = np.random.randint(0, 256, (300, 400, 3), dtype=np.uint8)
-    >>> resized_img = imresize(img, (200, 200), preserve_aspect_ratio=True, fill=255)
+    >>> resized_img = imresize(img, (200, 200), preserve_aspect_ratio=True, mode='constant', constant_values=255)
     """
     H, W = img.shape[:2]
-
     aspect_ratio_unchanged = (H / W == size[0] / size[1])
+
     if preserve_aspect_ratio and not aspect_ratio_unchanged:
-        
         scale = min(size[0] / H, size[1] / W)
         new_size = (int(W * scale), int(H * scale)) # (width, height)
-
         img = cv2.resize(img, new_size, interpolation=interpolation)
-
-        if img.shape[:2] != size:
-            dh = size[0] - img.shape[0]
-            dw = size[1] - img.shape[1]
-            padding = [(dh // 2, dh // 2), (dw // 2, dw // 2)]
-            padding.extend([(0, 0)] * (img.ndim - 2))  # No padding for channels
-            img = np.pad(img, padding, mode='constant', constant_values=fill)
-
-    # OpenCV uses (width, height) format
-    img = cv2.resize(img, size[::-1], interpolation=interpolation)
+        img = _pad_image_to_size(img, size, **kwargs)
+    else:
+        # OpenCV uses (width, height) format
+        img = cv2.resize(img, size[::-1], interpolation=interpolation)
 
     return img
 
@@ -149,3 +144,35 @@ def imresize_maximum(
         img = cv2.resize(img, new_size, interpolation=interpolation)
 
     return img
+
+
+def _pad_image_to_size(
+        img: np.ndarray,
+        size: tuple[int, int],
+        **kwargs: dict,
+    ) -> np.ndarray:
+        """
+        Pad the image to the specified size if it is smaller.
+
+        Parameters
+        ----------
+        img : ndarray
+            Input image to pad, must be a numpy array.
+        size : tuple[int, int]
+            Desired output size as (height, width).
+        **kwargs : dict
+            Additional keyword arguments to pass to np.pad.
+
+        Returns
+        -------
+        np.ndarray
+            Padded image as a numpy array.    
+        """
+        if img.shape[:2] != size:
+            dh = size[0] - img.shape[0]
+            dw = size[1] - img.shape[1]
+            padding = [(floor(dh / 2), ceil(dh / 2)), (floor(dw / 2), ceil(dw / 2))]
+            padding.extend([(0, 0)] * (img.ndim - 2))  # No padding for channels
+            img = np.pad(img, padding, **kwargs)
+        
+        return img
