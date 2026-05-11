ccc1b45223361ad004f257a5c7583614c21bbdc0
Atif Khurshid <31736261+atifkhurshid@users.noreply.github.com>
2026-05-11 10:33:13 +0200

    Update set_seed function

--- src/cvtools/utils/pytorch/random.py
+++ src/cvtools/utils/pytorch/random.py
@@ -4,10 +4,11 @@ Utility functions for random number generation.
 
 # Author: Atif Khurshid
 # Created: 2025-11-11
-# Modified: None
-# Version: 1.0
+# Modified: 2026-05-11
+# Version: 1.1
 # Changelog:
 #     - 2025-11-11: Added set_seed function.
+#     - 2026-05-11: Updated set_seed function to include torch.device type.
 
 import random
 
@@ -15,7 +16,7 @@ import torch
 import numpy as np
 
 
-def set_seed(seed: int, device: str = "cpu"):
+def set_seed(seed: int, device: torch.device = torch.device("cpu")) -> None:
     """
     Set the random seed for reproducibility.
 
@@ -23,14 +24,14 @@ def set_seed(seed: int, device: str = "cpu"):
     ----------
     seed : int
         The seed value to set.
-    device : str, optional
+    device : torch.device, optional
         The device type ("cpu" or "cuda"), by default "cpu".
     """
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
 
-    if device == "cuda":
+    if device == torch.device("cuda"):
         torch.cuda.manual_seed(seed)
         torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.deterministic = True
